edcd54e341c10bc31e3c7d7b6bf88d3cd446e105
heeun98 <phu98@naver.com>
2026-04-06 19:51:12 +0900

    chore: init

---
+++ src/main/java/com/team/peektime_api/PeektimeApiApplication.java
@@ -0,0 +1,13 @@
+package com.team.peektime_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeektimeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeektimeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/peektime_api/PeektimeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.peektime_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeektimeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

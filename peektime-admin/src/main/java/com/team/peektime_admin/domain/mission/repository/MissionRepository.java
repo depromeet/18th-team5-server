87ae91354ad8e7b6adeaf5fde8e9adfe4cff9ce9
heeun98 <phu98@naver.com>
2026-04-25 17:14:33 +0900

    feat: 미션풀 관리 페이지 추가 - templates/mission/pool.html: 미션 목록, 필터링, 등록/수정 모달 - MissionPageController: 미션 목록 조회, 페이징 - MissionRepository: 페이징 조회 메소드 추가

--- peektime-admin/src/main/java/com/team/peektime_admin/domain/mission/repository/MissionRepository.java
+++ peektime-admin/src/main/java/com/team/peektime_admin/domain/mission/repository/MissionRepository.java
@@ -1,9 +1,13 @@
 package com.team.peektime_admin.domain.mission.repository;
 
 import com.team.peektime_admin.domain.mission.entity.Mission;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
 
     long countByDeletedFalse();
+
+    Page<Mission> findAllByDeletedFalse(Pageable pageable);
 }

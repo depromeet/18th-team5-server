0b56141fd47cf536b2ee43cb6ddf25591b6517d1
heeun98 <phu98@naver.com>
2026-05-07 21:18:18 +0900

    chore: redis 설정 추가

--- peektime-api/src/main/java/com/team/peektime_api/PeektimeApiApplication.java
+++ peektime-api/src/main/java/com/team/peektime_api/PeektimeApiApplication.java
@@ -7,10 +7,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class PeektimeApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PeektimeApiApplication.class, args);
 	}
-
 
 }

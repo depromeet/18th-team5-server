50da7ceebb4650c052b52aa5ffb007c899a0332a
heeun98 <phu98@naver.com>
2026-05-08 00:32:23 +0900

    test

--- peektime-api/src/main/java/com/team/peektime_api/PeektimeApiApplication.java
+++ peektime-api/src/main/java/com/team/peektime_api/PeektimeApiApplication.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class PeektimeApiApplication {
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(PeektimeApiApplication.class, args);
 	}

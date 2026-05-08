082eecf5cab3ec225d0d37a09e3686c9f09d3ebc
oohyj <hyj0660@gmail.com>
2026-05-08 21:44:21 +0900

    refactor: UserType label 업데이트
    - NATURE_EXPLORER: 제철을 쫓는 탐험가
    - NEIGHBORHOOD_WALKER: 일상 속 제철 산책가
    - SEASONAL_GOURMET: 제철을 채우는 라이프 크리에이터
    - DAILY_OBSERVER: 제철을 음미하는 감상가

--- peektime-api/src/main/java/com/team/peektime_api/global/common/enums/UserType.java
+++ peektime-api/src/main/java/com/team/peektime_api/global/common/enums/UserType.java
@@ -6,10 +6,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum UserType {
-    NATURE_EXPLORER("자연 탐험가", "밖에서 적극적으로 활동하는 타입"),
-    NEIGHBORHOOD_WALKER("동네 산책러", "밖에서 가볍게 활동하는 타입"),
-    SEASONAL_GOURMET("제철 미식가", "실내에서 적극적으로 활동하는 타입"),
-    DAILY_OBSERVER("일상 관찰자", "실내에서 가볍게 활동하는 타입");
+    NATURE_EXPLORER("제철을 쫓는 탐험가", "밖에서 적극적으로 활동하는 타입"),
+    NEIGHBORHOOD_WALKER("일상 속 제철 산책가", "밖에서 가볍게 활동하는 타입"),
+    SEASONAL_GOURMET("제철을 채우는 라이프 크리에이터", "실내에서 적극적으로 활동하는 타입"),
+    DAILY_OBSERVER("제철을 음미하는 감상가", "실내에서 가볍게 활동하는 타입");
 
     private final String label;
     private final String description;

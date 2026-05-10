4d534d7486d877a6997e458c6f98aa7583a26d04
heeun98 <phu98@naver.com>
2026-05-10 16:30:38 +0900

    feat: Admin home API에 date 쿼리 파라미터 추가

--- peektime-admin/src/main/java/com/team/peektime_admin/domain/home/controller/HomeApiController.java
+++ peektime-admin/src/main/java/com/team/peektime_admin/domain/home/controller/HomeApiController.java
@@ -3,10 +3,14 @@ package com.team.peektime_admin.domain.home.controller;
 import com.team.peektime_admin.domain.home.dto.HomeDataResponse;
 import com.team.peektime_admin.domain.home.service.HomeDataService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/home")
@@ -16,7 +20,8 @@ public class HomeApiController {
     private final HomeDataService homeDataService;
 
     @GetMapping
-    public ResponseEntity<HomeDataResponse> getHomeData() {
-        return ResponseEntity.ok(homeDataService.getHomeData());
+    public ResponseEntity<HomeDataResponse> getHomeData(
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        return ResponseEntity.ok(homeDataService.getHomeData(date));
     }
 }

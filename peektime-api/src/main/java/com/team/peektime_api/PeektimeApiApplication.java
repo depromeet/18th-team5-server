0995aedc6a56fdb0f4ce36e5e004bab47ab9961b
heeun98 <phu98@naver.com>
2026-05-12 16:31:59 +0900

    feat: @EnableAsync 설정 추가

--- peektime-api/src/main/java/com/team/peektime_api/PeektimeApiApplication.java
+++ peektime-api/src/main/java/com/team/peektime_api/PeektimeApiApplication.java
@@ -3,9 +3,11 @@ package com.team.peektime_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
+@EnableAsync
 @EnableScheduling
 @SpringBootApplication
 public class PeektimeApiApplication {

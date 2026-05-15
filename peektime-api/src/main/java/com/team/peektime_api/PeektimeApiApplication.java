16aa3a1eac489a0edfcf534d39df4ebec7e482bd
heeun98 <phu98@naver.com>
2026-05-15 20:51:01 +0900

    chore: 포트 경로 변경

--- peektime-api/src/main/java/com/team/peektime_api/PeektimeApiApplication.java
+++ peektime-api/src/main/java/com/team/peektime_api/PeektimeApiApplication.java
@@ -12,8 +12,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class PeektimeApiApplication {
 
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(PeektimeApiApplication.class, args);
 	}

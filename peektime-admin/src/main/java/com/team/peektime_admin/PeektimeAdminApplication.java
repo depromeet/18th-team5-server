c9541958ac3404b824d66e217bc5912b3c3aa03b
heeun98 <phu98@naver.com>
2026-05-21 04:05:05 +0900

    chore: yml 수정

--- peektime-admin/src/main/java/com/team/peektime_admin/PeektimeAdminApplication.java
+++ peektime-admin/src/main/java/com/team/peektime_admin/PeektimeAdminApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @ConfigurationPropertiesScan
 public class PeektimeAdminApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(PeektimeAdminApplication.class, args);
 	}
